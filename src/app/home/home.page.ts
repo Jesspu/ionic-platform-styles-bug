47056159e8be01474f1030c6de267b81f62d7075
Jess Patton <patmj@FF-devs-MacBook-Pro-3.local>
2024-04-05 14:33:49 -0500

    reproduce issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,8 +12,6 @@ import {
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
     IonHeader,
@@ -25,6 +23,37 @@ import {
     IonList,
     IonItem,
   ],
+  template: `
+    <ion-header [translucent]="true">
+      <ion-toolbar>
+        <ion-title> Blank </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content [fullscreen]="false">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list>
+        <ion-item>
+          <ion-select label="Select Normal">
+            <ion-select-option></ion-select-option>
+          </ion-select>
+        </ion-item>
+        @if(showSelectInIf) {
+        <ion-item>
+          <ion-select label="Select In If">
+            <ion-select-option></ion-select-option>
+          </ion-select>
+        </ion-item>
+        }
+      </ion-list>
+    </ion-content>
+  `,
+  styles: [``],
 })
 export class HomePage {
   public showSelectInIf: boolean = false;
