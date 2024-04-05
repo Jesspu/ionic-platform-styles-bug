0c34aa9881549cf3964d4f791ca21fed79ca852d
Jess Patton <patmj@FF-devs-MacBook-Pro-3.local>
2024-04-05 13:27:56 -0500

    example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSelect,
+  IonSelectOption,
+  IonList,
+  IonItem,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSelect,
+    IonSelectOption,
+    IonList,
+    IonItem,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  public showSelectInIf: boolean = false;
+  constructor() {
+    setTimeout(() => {
+      this.showSelectInIf = true;
+    }, 1000);
+  }
 }
